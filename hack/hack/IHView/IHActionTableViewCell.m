5255296ac3c41ad736c73c215bdf95e0d9633fb8
x461909066 <xiaotongjoe@gmail.com>
2013-07-20 16:19:14 +0800

    tableviewcell

--- hack/hack/IHView/IHActionTableViewCell.m
+++ hack/hack/IHView/IHActionTableViewCell.m
@@ -16,17 +16,57 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-        bannerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.contentView.frame.size.width, 30)];
+        bannerView = [[UIView alloc] initWithFrame:CGRectMake(10, 0, self.contentView.frame.size.width, 30)];
         [bannerView setBackgroundColor:[UIColor clearColor]];
         titleLab = [[UILabel alloc] initWithFrame:bannerView.frame];
         titleLab.text = @"百度开放云编程马拉松";
+        titleLab.font = [UIFont systemFontOfSize:12];
+        [titleLab setBackgroundColor:[UIColor clearColor]];
         [bannerView addSubview:titleLab];
         [self.contentView addSubview:bannerView];
         
-        tipView = [[UIView alloc] initWithFrame:CGRectMake(0, 30, self.contentView.frame.size.width, self.contentView.frame.size.height - 30 - 10)];
+        tipView = [[UIView alloc] initWithFrame:CGRectMake(0, 30, self.contentView.frame.size.width, 90)];
+        photoView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 90, 90)];
+        photoView.image = [UIImage imageNamed:@"20115201212718777801.jpg"];
+        [tipView addSubview:photoView];
+        timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(120, 10, tipView.frame.size.width - 90, 20)];
+        timeLabel.text = @"时间：10-12 90:10 -- 10-14 10:11";
+        timeLabel.font = [UIFont systemFontOfSize:12];
+        [timeLabel setBackgroundColor:[UIColor clearColor]];
+        [tipView addSubview:timeLabel];
+        
+        localLabel = [[UILabel alloc] initWithFrame:CGRectMake(120, 30, tipView.frame.size.width - 90, 20)];
+        localLabel.text = @"地址：北京 东城区 东区 保利剧院";
+        localLabel.font = [UIFont systemFontOfSize:12];
+        [localLabel setBackgroundColor:[UIColor clearColor]];
+        [tipView addSubview:localLabel];
+        
+        classLabel = [[UILabel alloc] initWithFrame:CGRectMake(120, 50, tipView.frame.size.width - 90, 20)];
+        classLabel.text = @"类别：开发";
+        classLabel.font = [UIFont systemFontOfSize:12];
+        [classLabel setBackgroundColor:[UIColor clearColor]];
+        [tipView addSubview:classLabel];
+        
+        leaderLabel = [[UILabel alloc] initWithFrame:CGRectMake(120, 70, 90, 20)];
+        leaderLabel.text = @"joe 创建";
+        leaderLabel.font = [UIFont systemFontOfSize:12];
+        [leaderLabel setBackgroundColor:[UIColor clearColor]];
+        [tipView addSubview:leaderLabel];
+        
+        insteLabel = [[UILabel alloc] initWithFrame:CGRectMake(175, 70, 90, 20)];
+        insteLabel.text = @"3000 感兴趣";
+        insteLabel.font = [UIFont systemFontOfSize:12];
+        [insteLabel setBackgroundColor:[UIColor clearColor]];
+        [tipView addSubview:insteLabel];
+        
+        addedLabel = [[UILabel alloc] initWithFrame:CGRectMake(260, 70, 90, 20)];
+        addedLabel.text = @"900 参加";
+        addedLabel.font = [UIFont systemFontOfSize:12];
+        [addedLabel setBackgroundColor:[UIColor clearColor]];
+        [tipView addSubview:addedLabel];
+        [self.contentView setBackgroundColor:[UIColor colorWithRed:0 green:220 blue:0 alpha:0]];
         [tipView setBackgroundColor:[UIColor whiteColor]];
-        photoView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 60, 60)];
-        photoView.image = [UIImage imageNamed:@""];
+        [self.contentView addSubview:tipView];
         
     }
     return self;
--- hack/hack/ViewController.m
+++ hack/hack/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "IHActionTableViewCell.h"
 
 @interface ViewController ()
 
@@ -18,12 +19,45 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.frame];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    [self.view addSubview:tableView];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - table view methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 8;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 150;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *reuseableId = @"tableViewCell";
+    IHActionTableViewCell *cell = (IHActionTableViewCell *)[tableView dequeueReusableCellWithIdentifier:reuseableId];
+    if (cell == nil)
+    {
+        cell = [[IHActionTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseableId];
+    }
+    return cell;
 }
 
 @end
