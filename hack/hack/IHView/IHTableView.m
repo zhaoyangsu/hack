46e814c8682eed923814d072364965bc76d4e21e
x461909066 <xiaotongjoe@gmail.com>
2013-07-20 15:18:01 +0800

    wenjian jiegou

---
+++ hack/hack/IHView/IHTableView.m
@@ -0,0 +1,31 @@
+//
+//  IHTableView.m
+//  hack
+//
+//  Created by 靳晓童 on 13-7-20.
+//  Copyright (c) 2013年 ZhaoyangSu. All rights reserved.
+//
+
+#import "IHTableView.h"
+
+@implementation IHTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ hack/hack/myModel/IHAction.h
@@ -0,0 +1,13 @@
+//
+//  IHAction.h
+//  hack
+//
+//  Created by 靳晓童 on 13-7-20.
+//  Copyright (c) 2013年 ZhaoyangSu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IHAction : NSObject
+
+@end
